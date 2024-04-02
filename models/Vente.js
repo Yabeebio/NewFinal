a033c225cd03a0be6372ba5cde24a42547822622
Remy Ben Hamouda <remy.bnh@gmail.com>
2024-04-02 13:18:51 +0200

    oops

--- models/Vente.js
+++ models/Vente.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 
 const venteSchema = mongoose.Schema({
-    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
     vehicule: { type: String, required: true },
     immat: { type: String, required: true, unique: true },
     serie: { type: String, required: true, unique: true },
