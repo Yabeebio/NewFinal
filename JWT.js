8a11815e65f3518d274ae7dc2b9d9fca5bf26380
Remy Ben Hamouda <remy.bnh@gmail.com>
2024-04-02 13:38:30 +0200

    modif jwt.js

--- JWT.js
+++ JWT.js
@@ -1,4 +1,4 @@
-const { sign, verify } = require("jsonwebtoken");
+/* const { sign, verify } = require("jsonwebtoken");
 
 const createTokens = (user) => {
     const accessToken = sign({
@@ -19,6 +19,35 @@ const validateToken = (req, res, next) => {
         const validToken = verify(accessToken, "SECRET");
         if (validToken) {
             req.authenticated = true;
+            return next();
+        }
+    }
+    catch (error) {
+        return res.status(400).json({ error: error});
+    }
+};
+
+module.exports = { createTokens, validateToken }; */
+
+const { sign, verify } = require("jsonwebtoken");
+
+const createTokens = (user) => {
+    const accessToken = sign({
+        id: user._id,
+        email: user.email,
+        admin: user.admin
+    }, "SECRET");
+    return accessToken;
+};
+
+const validateToken = (req, res, next) => {
+    const accessToken = req.cookies["access_token"];
+    if (!accessToken)
+        return res.status(400).json({ error: "User not authenticated" })
+    try {
+        const validToken = verify(accessToken, "SECRET");
+        if (validToken) {
+            req.user = validToken; // Ajouter les informations de l'utilisateur à req.user
             return next();
         }
     }
